4b52716f777f23960a28d91857e4109a26ccc3aa
gilangdhan55 <gilangemailsalah@gmail.com>
2025-05-05 11:20:36 +0700

    slider for habib:

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+/*=============== EXPANDED LIST ===============*/
+const navExpand = document.getElementById('nav-expand'),
+      navExpandList = document.getElementById('nav-expand-list'), 
+      navExpandIcon = document.getElementById('nav-expand-icon')
+
+navExpand.addEventListener('click', () => {
+   // Expand list
+   navExpandList.classList.toggle('show-list')
+
+   // Rotate icon
+   navExpandIcon.classList.toggle('rotate-icon')
+})
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+const arr = ['1', '2', '3'];
+
+for(const item of arr) {
+   const foto = document.createElement("img");
+   foto.src = `${item}.jpg`;
+   foto.alt = 'Gambar dari JavaScript';
+   document.getElementById(`img${item}`).appendChild(foto);
+} 
